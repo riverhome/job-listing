623fd8c6afe7709dfc56f9158ccfe7ece56212f5
riverhome <liushuijiaxiang@gmail.com>
2017-04-21 20:09:03 +0800

    better welcome/index & admin/jobs/index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_many :resumes
-   def admin?
-     is_admin
-   end
-end
+      has_many :resumes
+        def admin?
+         is_admin
+        end
+      end
